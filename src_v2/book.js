3ffeedac9924662342deb4545d97110d6518d66e
Austin Johnson <technicallyaustin@gmail.com>
2023-02-03 20:27:28 -0700

    prepping more changes

--- src_v2/book.js
+++ src_v2/book.js
@@ -7,13 +7,20 @@ function book(title, author, pages, read){
 }
 
 // Prototype Methods
+Book.prototype.info = function(){}
+Book.prototype.io = function(){}
 
 function bookOptions(){
     const myLibrary = []
+    let library; // jquery for libray div
 // DOM selection
-    const newBook = {};
-    const addToLibrary = {};
-    const createBooks = {};
+    const newBook = {}; // creates a new book object
+    const addToLibrary = {}; // adds the created book object to the library if it doesn't exist
+    const newDomBook = (Book) => {}; // given a Book object, creats a Dom card
+    const createLibrary = {
+    // iterate myLibrary[]
+        // run newDomBook with each Library Book Object
+    };
 }
 
 
