a35f95bda4dc8467d8e02da9321a31fac5e877f3
Austin Johnson <technicallyaustin@gmail.com>
2023-02-04 23:59:02 -0700

    git lost on folder

---
+++ src_v2/api_v2 2.js
@@ -0,0 +1,22 @@
+// creates Semantic HTML elements
+function createLibrary(){
+    // Header
+    const headerElements = {
+        header: function(){}, // creates header container
+        nav: function(){} // creates nave Bar and Nav elements
+    }
+
+    // Main
+    const mainElements = {
+        main: function(){}, // creates the main with class Library
+        section: function(){}, // creates a section with class shelf
+        article: function(){}, // create an aritcle per Book with class book
+        aside: function(){} //  defines Form and form creation button
+    }
+
+    // Footer
+    const footerElements = {
+        footer: function(){}, // creates the footer element
+        info: function(){} // creates the info within the footer.
+    }
+}
