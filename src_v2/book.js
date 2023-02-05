8d72a14dbe0ef270e080fbc48a0dc4cdde4bca7e
tehcnicallyAustin <tehcnicallyAustin@gmail.com>
2023-02-04 18:57:49 -0700

    working through precreating objects for Book, Form and semantic API

--- src_v2/book.js
+++ src_v2/book.js
@@ -1,5 +1,5 @@
 // handles book creation and Book logical operations
-function book(title, author, pages, read){
+function Book(title, author, pages, read){
     title = this.title
     author = this.author
     pages = this.pages
@@ -8,20 +8,39 @@ function book(title, author, pages, read){
 
 // Prototype Methods
 Book.prototype.info = function(){}
-Book.prototype.io = function(){}
+Book.prototype.id = function(){}
 
-function bookOptions(){
-    const myLibrary = []
-    let library; // jquery for libray div
-// DOM selection
-    const newBook = {}; // creates a new book object
-    const addToLibrary = {}; // adds the created book object to the library if it doesn't exist
-    const newDomBook = (Book) => {}; // given a Book object, creats a Dom card
-    const createLibrary = {
-    // iterate myLibrary[]
-        // run newDomBook with each Library Book Object
-    };
+// handles form logical operations
+function form(){
+    const form = document.querySelector(".new-book")
+
+    function formOperations(){
+        const formOptions = {
+            add: function(){}, // adds an event listener to the form that will create the form
+            form: function(){}, // creates the form element with class new-book
+            prevent: function(){}, // prevents the default submission for the form
+            legend: function(){}, // title of the new book form
+            title: function(){}, // Book title input
+            author: function(){}, // Book author input
+            pages: function(){}, // Book pages input
+            read: function(){}, // book read boolean input
+            submit: function(){} // adds event listener that saves the information from form to create Book
+        }
+    }
 }
 
+// Book Logic
+function bookOperations(){
+    const myLibrary = [];
+    const library = document.querySelector(".library")
+
+    const options = {
+        new: function(){}, // given Form input creates a Book Object
+        save: function(){}, // after a Book has been created, adds to Library
+        dom: function(){}, //  create a Dom Book Element from a Book Obj
+        remove: function(){}, // remove the Book from the library and the DOM
+        library: function(){} // create the library of books using bookOptions.dom
+    }
+}
 
 
