45faf75449a173e7cee9e30b8ca6e524283c566f
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-02-04 20:41:33 -0700

    added header and nav

--- src_v2/api_v2.js
+++ src_v2/api_v2.js
@@ -2,13 +2,24 @@
 function createLibrary(){
     // Header
     const headerElements = {
-        header: function(){}, // creates header container
-        nav: function(){} // creates nave Bar and Nav elements
-    }
+        header: function(){
+            const head = document.body.appendChild(document.createElement("header"));
+            this.head = head
+        }, // creates header container
+        navbar: function(){
+            const nav = this.head.appendChild(document.createElement("navbar"));
+            const text = nav.appendChild(document.createElement("p"));
+            text.textContent = "Teechnically Library"
+        } // creates nav Bar and Nav elements
+    };
+    headerElements.header()
+    headerElements.navbar()
 
     // Main
     const mainElements = {
-        main: function(){}, // creates the main with class Library
+        main: function(){
+            
+        }, // creates the main with class Library
         section: function(){}, // creates a section with class shelf
         article: function(){}, // create an aritcle per Book with class book
         aside: function(){} //  defines Form and form creation button
@@ -19,4 +30,4 @@ function createLibrary(){
         footer: function(){}, // creates the footer element
         info: function(){} // creates the info within the footer.
     }
-}
+}; createLibrary()
