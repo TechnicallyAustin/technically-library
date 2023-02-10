a29e0da3a2cf9c7eea9d049920dadaa755b797f1
tehcnicallyAustin <Technicallyaustin@gmail.com>
2023-02-10 15:27:50 -0700

    working on encorporating the library

--- src_v2/final.js
+++ src_v2/final.js
@@ -1,19 +1,24 @@
-//Lib
-
 // Book constructor w/ form Inputs
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-
-}
+};
 Book.prototype.info = function () {};
 Book.prototype.id = function () {};
 
+// Library Object
+const library = {
+    lib: [],
+    addBook: function(book){
+        this.lib.push(book)
+    },
+    removeBook: function(){},
+}
+
 
 // Card object
-
 const cards = {
     card: null,
     newCard: function(){
@@ -50,37 +55,70 @@ const cards = {
         pages.setAttribute("class", "pages")
     },
     read: function(){
-        const read = this.footer.appendChild(document.createElement("p");
+        const read = this.footer.appendChild(document.createElement("p"));
         read.setAttribute("class", "read")
     },
     remove: function(){
         const remove = this.footer.appendChild(document.createElement("button"))
-        remove.
         remove.setAttribute("id", "remove-book")
-        remove.setAttribute("value", "remove")
+        remove.setAttribute("value", "Remove")
+    },
+    add: function(){
+        this.newCard()
+        this.header()
+        this.body()
+        this.title()
+        this.text()
+        this.footer()
+        this.pages()
+        this.read()
+        this.remove()
     }
-}
+}; cards.add()
 
+// Form Object
 const form = {
     form: null,
-    newForm: function(){},
-    prevent: function(){},
-    legend: function(){},
-    inputs: function(){},
-    submit: function(){},
-    add: function(){}
-}
+    newForm: function(){
+        const container = document.querySelector(".new-book-container")
+        const newForm = container.appendChild(document.createElement("form"))
+        newForm.setAttribute("class", "book-form")
+        this.form = newForm
+    },
+    prevent: function(){
+        this.form.addEventListener("submit", function(event){
+            event.preventDefault()
+        })
+    },
+    legend: function(){
+        const legend = this.form.appendChild(document.createElement("legend"))
+        legend.setAttribute("class", "legend")
+    },
+    inputs: function(){
+        const inputs = ["Title", "Author", "Pages", "Read"]
+        for (let i = 0; i < inputs.length; i++){
+            function fieldset(){}
+            function label(){}
+            function input(){}
+        }
+    },
+    submit: function(){
+        const submit = this.form.appendChild(document.createElement("button"));
+        submit.setAttribute("id", "submit-book")
+        submit.setAttribute("class", "submit-book")
+        submit.setAttribute("value", "Add Book")
+
+        submit.addEventListener("click", () => {
+
+        })
+    },
+    add: function(){
+        this.newForm()
+        this.prevent()
+        this.legend()
+        this.inputs()
+        this.submit()
+    }
+}; form.add()
 
 
-
-// form constructor
-
-    // form
-        // inputs
-        // submit
-        
-
-// card constructor w/ book properties
-
-    // card
-        // 
